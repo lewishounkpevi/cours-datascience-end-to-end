f1008a6935484a47d1c0a992765ded5d5455cb32
Lewis <dumesnil.hounkpevi@gmail.com>
2024-07-07 17:30:18 +0200

    deploiement api avec versionnement des modeles avec mlflow

---
+++ docker-supervised-classification/src/model_evaluation_mlflow.py
@@ -0,0 +1,76 @@
+import shap
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+import mlflow
+import mlflow.sklearn
+
+def evaluate_model(model, X_test, y_test, model_name, run_id):
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    report = classification_report(y_test, predictions, output_dict=True)
+    cm = confusion_matrix(y_test, predictions)
+    
+    # Log metrics and confusion matrix with MLflow
+    with mlflow.start_run(run_id=run_id):
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_dict(report, "classification_report.json")
+        mlflow.log_dict(cm.tolist(), "confusion_matrix.json")
+    
+    return accuracy, report, cm
+
+def plot_confusion_matrix(cm, class_names):
+    plt.figure(figsize=(10, 7))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+# def plot_shap_values(model, X_train, feature_names):
+#     explainer = shap.Explainer(model)
+#     shap_values = explainer(X_train)
+
+    # Summary plot
+    # shap.summary_plot(shap_values, X_train, feature_names=feature_names)
+
+    # # Dependence plot for the first feature
+    # shap.dependence_plot(0, shap_values, X_train, feature_names=feature_names)
+
+    # # Force plot for the first sample
+    # shap.force_plot(explainer.expected_value, shap_values[0,:], X_train.iloc[0,:], matplotlib=True)
+    # plt.show()
+
+if __name__ == "__main__":
+    from data_preprocessing import load_data, preprocess_data, split_data
+    from model_training_mlflow import train_xgboost, train_random_forest, train_knn
+
+    # Charger et prétraiter les données
+    data = load_data('https://raw.githubusercontent.com/lewishounkpevi/cours-datascience-end-to-end/main/docker-supervised-classification/data/diabetes.csv')
+    data = preprocess_data(data)
+    X_train, X_test, y_train, y_test = split_data(data, target_column='Outcome')
+
+    # Entraîner les modèles avec recherche d'hyperparamètres
+    xgboost_model, xgboost_run_id = train_xgboost(X_train, y_train)
+    rf_model, rf_run_id = train_random_forest(X_train, y_train)
+    knn_model, knn_run_id = train_knn(X_train, y_train)
+
+    # Évaluer les modèles
+    xgboost_accuracy, xgboost_report, xgboost_cm = evaluate_model(xgboost_model, X_test, y_test, "XGBoost Model", xgboost_run_id)
+    rf_accuracy, rf_report, rf_cm = evaluate_model(rf_model, X_test, y_test, "Random Forest Model", rf_run_id)
+    knn_accuracy, knn_report, knn_cm = evaluate_model(knn_model, X_test, y_test, "KNN Model", knn_run_id)
+
+    print(f"XGBoost Accuracy: {xgboost_accuracy}")
+    print(f"XGBoost Classification Report:\n{xgboost_report}")
+    plot_confusion_matrix(xgboost_cm, class_names=data.columns[:-1])
+
+    print(f"Random Forest Accuracy: {rf_accuracy}")
+    print(f"Random Forest Classification Report:\n{rf_report}")
+    plot_confusion_matrix(rf_cm, class_names=data.columns[:-1])
+
+    print(f"KNN Accuracy: {knn_accuracy}")
+    print(f"KNN Classification Report:\n{knn_report}")
+    plot_confusion_matrix(knn_cm, class_names=data.columns[:-1])
+
+    # Visualiser les valeurs SHAP pour XGBoost
+    # plot_shap_values(xgboost_model, X_train, feature_names=X_train.columns)
---
+++ docker-supervised-classification/src/model_training_mlflow.py
@@ -0,0 +1,78 @@
+import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+import mlflow
+import mlflow.sklearn
+
+def train_xgboost(X_train, y_train):
+    param_grid = {
+        'learning_rate': [0.01, 0.1, 0.2],
+        'max_depth': [3, 5, 7],
+        'n_estimators': [100, 200, 300]
+    }
+    model = xgb.XGBClassifier()
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=2)
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
+    
+    with mlflow.start_run() as run:
+        mlflow.set_tag("model", "xgboost")
+        mlflow.log_params(grid_search.best_params_)
+        mlflow.sklearn.log_model(best_model, "xgboost_model",
+                                 registered_model_name="xgboost_tracking")
+    
+    return best_model, run.info.run_id
+
+def train_random_forest(X_train, y_train):
+    param_grid = {
+        'n_estimators': [100, 200, 300],
+        'max_depth': [None, 10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+    model = RandomForestClassifier()
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=2)
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
+    
+    with mlflow.start_run() as run:
+        mlflow.set_tag("model", "random_forest")
+        mlflow.log_params(grid_search.best_params_)
+        mlflow.sklearn.log_model(best_model, "random_forest_model",
+                                 registered_model_name="rf_tracking")
+    
+    return best_model, run.info.run_id
+
+def train_knn(X_train, y_train):
+    param_grid = {
+        'n_neighbors': [3, 5, 7],
+        'weights': ['uniform', 'distance'],
+        'metric': ['euclidean', 'manhattan', 'minkowski']
+    }
+    model = KNeighborsClassifier()
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=2)
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
+    
+    with mlflow.start_run() as run:
+        mlflow.set_tag("model", "knn")
+        mlflow.log_params(grid_search.best_params_)
+        mlflow.sklearn.log_model(best_model, "knn_model",
+                                 registered_model_name="knn_tracking")
+    
+    return best_model, run.info.run_id
+
+if __name__ == "__main__":
+    from data_preprocessing import load_data, preprocess_data, split_data
+    
+
+    # Charger et prétraiter les données
+    data = load_data('https://raw.githubusercontent.com/lewishounkpevi/cours-datascience-end-to-end/main/docker-supervised-classification/data/diabetes.csv')
+    data = preprocess_data(data)
+    X_train, X_test, y_train, y_test = split_data(data, target_column='Outcome')
+
+    # Entraîner les modèles avec recherche d'hyperparamètres
+    xgboost_model, xgboost_run_id = train_xgboost(X_train, y_train)
+    rf_model, rf_run_id = train_random_forest(X_train, y_train)
+    knn_model, knn_run_id = train_knn(X_train, y_train)
